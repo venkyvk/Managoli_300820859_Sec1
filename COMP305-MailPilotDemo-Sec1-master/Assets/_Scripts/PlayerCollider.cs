2dfb7e0f09654db390f126d3cb35550e1ea69780
venkyvk <venka.managoli@gmail.com>
2015-09-28 20:11:24 -0400

    Mail Pilot
    Mail Pilot

--- COMP305-MailPilotDemo-Sec1-master/Assets/_Scripts/PlayerCollider.cs
+++ COMP305-MailPilotDemo-Sec1-master/Assets/_Scripts/PlayerCollider.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class PlayerCollider : MonoBehaviour {
 
+    //private instance variables
+    private AudioSource[] _audiosources; //array of audio sources
+    private AudioSource _cloudAudioSource, _islandAudioSource;
+
+
 	// Use this for initialization
 	void Start () {
-	
+        this._audiosources = this.GetComponents<AudioSource>();
+        this._cloudAudioSource = this._audiosources[1];
+        this._islandAudioSource = this._audiosources[2];
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnTriggerEnter2D(Collider2D otherGameObject)
+    {
+        if(otherGameObject.tag == "Island")
+        {
+            this._islandAudioSource.Play(); //Play the Yay sound
+        }
+        if (otherGameObject.tag == "Cloud")
+        {
+            this._cloudAudioSource.Play(); //PLay the thunder sound
+        }
+    }
 }
